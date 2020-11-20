354e392ab7c53972f50fb34fc35bec132079313c
xuan-123 <18735495269@163.com>
2020-11-20 15:19:14 +0800

    删除无用文件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+ 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
